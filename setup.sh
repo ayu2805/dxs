36b9f69f58ca636988d97856eb73931d472e4396
Ayushmaan Padhi <76616749+ayu2805@users.noreply.github.com>
2024-05-10 20:48:58 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -118,7 +118,7 @@ fi
 
 echo ""
 read -r -p "Do you want to install Cloudflare Warp? [y/N] " response
-if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]; the
+if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]; then
     curl -fsSL https://pkg.cloudflareclient.com/pubkey.gpg | sudo gpg --yes --dearmor --output /usr/share/keyrings/cloudflare-warp-archive-keyring.gpg
     echo "deb [signed-by=/usr/share/keyrings/cloudflare-warp-archive-keyring.gpg] https://pkg.cloudflareclient.com/ $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/cloudflare-client.list
     sudo apt update && sudo apt install cloudflare-warp
