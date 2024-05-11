ff3fa8d8b6a47b0dfb01c54baac6bc359ecadf20
Ayushmaan Padhi <76616749+ayu2805@users.noreply.github.com>
2024-05-11 17:48:28 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -75,7 +75,7 @@ if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]; then
     git config --global user.email "$git_email"
     ssh-keygen -t ed25519 -C "$git_email"
     git config --global gpg.format ssh
-    git config --global user.signingkey /home/$un/.ssh/id_ed25519.pub
+    git config --global user.signingkey /home/$whoami/.ssh/id_ed25519.pub
     git config --global commit.gpgsign true
 fi
 
