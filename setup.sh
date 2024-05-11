6e8d797981e15d97a261e62ddad29ba4b3025503
Ayushmaan Padhi <76616749+ayu2805@users.noreply.github.com>
2024-05-11 16:43:21 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -81,7 +81,7 @@ xfconf-query -c xfce4-notifyd -p  /do-slideout -n -t bool -s true
 xfconf-query -c xfce4-notifyd -p  /notify-location -n -t int -s 3
 xfconf-query -c xfce4-notifyd -p  /expire-timeout -n -t int -s 5
 xfconf-query -c xfce4-notifyd -p  /initial-opacity -n -t double -s 1
-xfconf-query -c thunar -p misc-exec-shell-scripts-by-default -n -t bool -set true
+xfconf-query -c thunar -p misc-exec-shell-scripts-by-default -n -t bool -s true
 sudo sed -i 's/^#greeter-setup-script=.*/greeter-setup-script=\/usr\/bin\/numlockx on/' /etc/lightdm/lightdm.conf
 sudo cp lightdm-gtk-greeter.conf /etc/lightdm/
 sudo systemctl enable lightdm.service
