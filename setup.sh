ed611359df1bb357064c688f3cf0a67feaa12589
Ayushmaan Padhi <76616749+ayu2805@users.noreply.github.com>
2024-05-14 21:04:51 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -75,9 +75,29 @@ echo ""
 echo "Installing XFCE..."
 echo ""
 sudo apt install -y $(cat xfce)
+xfconf-query -c xsettings -p /Net/IconThemeName -n -t string -s "Papirus-Dark"
+xfconf-query -c xsettings -p /Net/ThemeName -n -t string -s "Colloid-Dark"
+xfconf-query -c xfwm4 -p /general/button_layout -n -t string -s "|HMC"
+xfconf-query -c xfwm4 -p /general/theme -n -t string -s "Colloid-Dark"
+xfconf-query -c xfwm4 -p /general/raise_with_any_button -n -t bool -s false
+xfconf-query -c xfwm4 -p /general/mousewheel_rollup -n -t bool -s false
+xfconf-query -c xfwm4 -p /general/scroll_workspaces -n -t bool -s false
+xfconf-query -c xfwm4 -p /general/placement_ratio -n -t int -s 100
+xfconf-query -c xfwm4 -p /general/show_popup_shadow -n -t bool -s true
+xfconf-query -c xfwm4 -p /general/wrap_windows -n -t bool -s false
+xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/image-path -n -t string -s "/usr/share/backgrounds/xfce/xfce-leaves.svg"
+xfconf-query -c xfce4-panel -p /panels -n -t int -s 1 -a
+xfconf-query -c xfce4-panel -p /panels/panel-1/size -n -t int -s 32
+xfconf-query -c xfce4-panel -p /panels/panel-1/icon-size -n -t int -s 16
+xfconf-query -c xfce4-panel -p /plugins/plugin-1/show-button-title -n -t bool -s false
+xfconf-query -c xfce4-panel -p /plugins/plugin-1/button-icon -n -t string -s "desktop-environment-xfce"
+xfconf-query -c xfce4-desktop -p /desktop-icons/file-icons/show-filesystem -n -t bool -s false
+xfconf-query -c xfce4-notifyd -p  /do-slideout -n -t bool -s true
+xfconf-query -c xfce4-notifyd -p  /notify-location -n -t int -s 3
+xfconf-query -c xfce4-notifyd -p  /expire-timeout -n -t int -s 5
+xfconf-query -c xfce4-notifyd -p  /initial-opacity -n -t double -s 1
 sudo sed -i 's/^#greeter-setup-script=.*/greeter-setup-script=\/usr\/bin\/numlockx on/' /etc/lightdm/lightdm.conf
 sudo cp lightdm-gtk-greeter.conf /etc/lightdm/
-sudo systemctl enable lightdm.service
 
 echo ""
 read -r -p "Do you want to configure git? [y/N] " response
@@ -147,8 +167,6 @@ fi
 
 mkdir -p ~/.config/
 cp QtProject.conf ~/.config/
-xdg-user-dirs-update
-cp "XFCE Setup" ~/Desktop/
 
 echo ""
 read -r -p "Do you want to reboot (recommended)? [y/N] " response
