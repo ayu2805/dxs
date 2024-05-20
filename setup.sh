167fc3ef35762f99cd825c335c29ca335948a770
Ayushmaan Padhi <76616749+ayu2805@users.noreply.github.com>
2024-05-20 09:45:00 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -115,6 +115,16 @@ if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]; then
 fi
 
 echo ""
+read -r -p "Do you want to install Firefox? [y/N] " response
+if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]; then
+    sudo install -d -m 0755 /etc/apt/keyrings
+    wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- | sudo tee /etc/apt/keyrings/packages.mozilla.org.asc > /dev/null
+    echo "deb [signed-by=/etc/apt/keyrings/packages.mozilla.org.asc] https://packages.mozilla.org/apt mozilla main" | sudo tee -a /etc/apt/sources.list.d/mozilla.list > /dev/null
+    sudo apt update
+    sudo apt install -y firefox
+fi
+
+echo ""
 read -r -p "Do you want to install Chromium? [y/N] " response
 if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]; then
     sudo apt install -y chromium
